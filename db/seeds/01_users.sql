f687c08a1589319861de584e050c4c51e3aa1210
Sydney Sisco <sydsisco@gmail.com>
2021-03-05 20:07:53 -0800

    fix SQL syntax in schema and seed files

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,19 +1,19 @@
-INSERT INTO users (first_name, last_name, email, password)
-VALUES ('Hosam', 'Dahrooge', 'hosam_dahrooge@hotmail.com', 'password'),
-VALUES ('Connie', 'Ho', 'connie_ho@gmail.com', 'password'),
-VALUES ('Sydney', 'Sisco', 'sydney_sisco@yahoo.com', 'password');
+INSERT INTO users (first_name, last_name, email, password) VALUES
+('Hosam', 'Dahrooge', 'hosam_dahrooge@hotmail.com', 'password'),
+('Connie', 'Ho', 'connie_ho@gmail.com', 'password'),
+('Sydney', 'Sisco', 'sydney_sisco@yahoo.com', 'password');
 
-INSERT INTO categories (name)
-VALUES ('Watch'),
-VALUES ('Eat'),
-VALUES ('Read'),
-VALUES ('Buy'),
-VALUES ('Uncatergorized');
+INSERT INTO categories (name) VALUES
+('Watch'),
+('Eat'),
+('Read'),
+('Buy'),
+('Uncatergorized');
 
-INSERT INTO items (name, user_id, category_id)
-VALUES ('Mulan', 1, 1),
-VALUES ('Banana', 3, 2),
-VALUES ('Sleep', 2, 5),
-VALUES ('80/20 Principle', 1, 3),
-VALUES ('Socks', 3, 4),
-VALUES ('Finish this module', 2, 5);
+INSERT INTO items (name, user_id, category_id) VALUES
+('Mulan', 1, 1),
+('Banana', 3, 2),
+('Sleep', 2, 5),
+('80/20 Principle', 1, 3),
+('Socks', 3, 4),
+('Finish this module', 2, 5);

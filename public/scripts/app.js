7ffa7420ab8a2f40c5e414746ddd38b6585d26c8
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-03-06 13:15:16 -0700

    requiring axios

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,35 @@
+const axios = require('axios');
+
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  $('#submit-btn').on('click', (event) => {
+    event.preventDefault();
+    const inputStr = $('#item-input').val();
+    console.log($('#category-container').children());
+    $('.items-list').append(`<div>
+    <button class='complete-btn'>COMPLETE</button>
+    <li>${inputStr}</li>
+    <button class='delete-btn'>DELETE</button>
+    </div>`);
+    deleteBtnListener();
+    completeBtnListener();
+  });
+
+
+
 });
+
+const deleteBtnListener = () => {
+  $('.delete-btn').on('click', (event) => {
+    $(event.target).parent().remove();
+  });
+};
+
+const completeBtnListener = () => {
+  $('.complete-btn').on('click', (event) => {
+    $($(event.target).parent().parent().parent().siblings().children('ul')).append($(event.target).parent());
+
+    $(event.target).toggleClass('complete-fill');
+    $(event.target).next().toggleClass('complete-strike');
+    $(event.target).next().next().toggleClass('complete-gray');
+  });
+};

ac29b15522a79db6cb60a8834c8c676e952baab2
Connie Ho <connie3@ualberta.ca>
2021-03-06 23:35:07 +0000

    refactor by splitting up db and routes

--- routes/items.js
+++ routes/items.js
@@ -8,122 +8,65 @@ const { categorizer } = require('../helpers/categorizer');
 
 const express = require('express');
 const router  = express.Router();
-
-module.exports = (db) => {
-  //gets all items for that user
-  router.get("/", (req, res) => {
-    const userId = req.session.user_id;
-    if (!userId) {
-      res.send('Not logged in!');
-      return;
-    }
-
-    const text = `
-    SELECT * FROM items
-    WHERE user_id = $1`;
-    const values = [userId];
-
-    db.query(text, values)
-      .then(data => {
-        const items = data.rows;
-        res.json(items); // replace with res.render(__.ejs, data.rows) when home page complete
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-  router.post("/", (req, res) => {
-    const userId = req.session.user_id;
-
-    if (!userId) {
-      res.send('Not logged in!');
-      return;
-    }
-    // replace category id with whatever is passed from client
-    categorizer(req.body.item).then((data) => {
-      const values = [userId, data, req.body.item];
-      let text = `
-      INSERT INTO items (user_id, category_id, name)
-      VALUES ($1, $2, $3) RETURNING *;`
-
-      db.query(text, values)
-        .then(data => {
-          res.json(data.rows[0]);
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-    });
-  });
+const { getUsersItems, addItem, editItem, deleteItem } = require('../db/item-queries.js');
 
 
-  router.patch("/:id", (req, res) => {
-    const userId = req.session.user_id;
+router.get("/", (req, res) => {
+  const userId = req.session.user_id;
+  if (!userId) {
+    res.send('Not logged in!');
+    return;
+  }
 
-    if (!userId) {
-      res.send('Not logged in!');
-      return;
-    }
+  getUsersItems(userId)
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ error: err.message }));
 
-    const values = [];
-    let text = `
-    UPDATE items
-    SET`
+});
 
-    if (req.body.name) {
-      values.push(req.body.name);
-      text += `name = $${values.length}`;
-    }
+router.post("/", (req, res) => {
+  const userId = req.session.user_id;
 
-    if (req.body.done) {
-      values.push(req.body.done);
-      text += `done = $${values.length}`;
-    }
+  if (!userId) {
+    res.send('Not logged in!');
+    return;
+  }
 
-    values.push(userId, Number(req.params.id));
-    text += `WHERE user_id = $${values.length - 1} AND id = $${values.length} RETURNING *`;
+  categorizer(req.body.item)
+    .then(data => addItem(userId, data, req.body.item))
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ error: err.message }));
 
-    db.query(text, values)
-      .then(data => {
-        res.json(data.rows[0]);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
+});
+
+router.patch("/:id", (req, res) => {
+  const userId = req.session.user_id;
+
+  if (!userId) {
+    res.send('Not logged in!');
+    return;
+  }
+
+  editItem({...req.body, userrId: userId, itemId: Number(req.params.id)})
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.delete("/:id", (req, res) => {
+  const userId = req.session.user_id;
+
+  if (!userId) {
+    res.send('Not logged in!');
+    return;
+  }
+
+  deleteItem(userId, Number(req.params.id))
+    .then(data => res.send(data))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
 
 
-  router.delete("/:id", (req, res) => {
-    const userId = req.session.user_id;
 
-    if (!userId) {
-      res.send('Not logged in!');
-      return;
-    }
+module.exports = router;
 
-    let text = `
-    DELETE FROM items
-    WHERE user_id = $1 AND id = $2 RETURNING *`;
 
-    const values = [userId, Number(req.params.id)];
-
-    db.query(text, values)
-      .then(data => {
-        res.send('Deleted');
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-  return router;
-};

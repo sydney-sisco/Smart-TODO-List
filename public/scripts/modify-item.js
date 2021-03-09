c4aa73277dd6b3d454f0ebae10ca780ae76a81b9
Connie Ho <connie3@ualberta.ca>
2021-03-09 18:04:24 +0000

    add pop up card

--- public/scripts/modify-item.js
+++ public/scripts/modify-item.js
@@ -1,5 +1,5 @@
 let itemId = null;
-let num = null
+let num = null;
 
 const updateItemNameHandler = function(e) {
   e.preventDefault();
@@ -68,46 +68,43 @@ $(() => {
     num = itemId.split('item-id-')[1]
 
     $('body').append(`
-    <div class="mod-items-wrapper">
-    <div id="modify-item-form">
-    <div>
-      <form id="edit-item-form" method="PATCH" action="/items/${num}">
-        <input type="text" name="name" placeholder="Click & Type to Update"></input>
-        <button form="edit-item-form" type="submit">EDIT</button>
-      </form>
-    </div>
-    <div>
-      <form id="delete-item-form" method="DELETE" action="/items/${num}"">
-        <label for="DELETE ME">DELETE ME</label>
-        <button form="delete-item-form" type="submit">DELETE</button>
-      </form>
-    </div>
-    <div>
-      <form id="change-cat-form" method="PATCH" action="/items/${num}"">
-        <div class="dropdown">
-          <a class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Change Categories
-          </a>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" name="1" href="#">Watch</a>
-            <a class="dropdown-item" name="2" href="#">Eat</a>
-            <a class="dropdown-item" name="3" href="#">Read</a>
-            <a class="dropdown-item" name="4" href="#">Buy</a>
-            <a class="dropdown-item" name="5" href="#">General</a>
-          </div>
+    <div class="mod-items-wrapper card">
+        <div id="modify-item-form card-body">
+        <div>
+          <form id="edit-item-form" method="PATCH" action="/items/${num}">
+            <input type="text" name="name" placeholder="Click & Type to Update"></input>
+            <button class="submit-btn" form="edit-item-form" type="submit">EDIT</button>
+          </form>
         </div>
-        <input type="text" name="category_id" placeholder="CATEGORY ID BUT WILL BE A DROP DOWN LATER"></input>
-        <button form="change-cat-form" type="submit">CHANGE</button>
-      </form>
-    </div>
-    <form id="cancel-modify">
-      <button form="cancel-modify">Cancel</button>
-    </form>
-    </div>
-    </div>
+        <div>
+          <form id="delete-item-form" method="DELETE" action="/items/${num}"">
+            <label for="DELETE ME">DELETE ME</label>
+            <button class="submit-btn" form="delete-item-form" type="submit">DELETE</button>
+          </form>
+        </div>
+        <div>
+          <form id="change-cat-form" method="PATCH" action="/items/${num}"">
+            <div class="dropdown">
+              <a class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Change Categories
+              </a>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" name="1" href="#">Watch</a>
+                <a class="dropdown-item" name="2" href="#">Eat</a>
+                <a class="dropdown-item" name="3" href="#">Read</a>
+                <a class="dropdown-item" name="4" href="#">Buy</a>
+                <a class="dropdown-item" name="5" href="#">General</a>
+              </div>
+            </div>
+            <input type="text" name="category_id" placeholder="CATEGORY ID BUT WILL BE A DROP DOWN LATER"></input>
+            <button class="submit-btn" form="change-cat-form" type="submit">CHANGE</button>
+          </form>
+        </div>
+        <button class="cancel-btn far fa-window-close"></button>
+        </div>
     `);
 
-    $('.body-container').css('filter','blur(5px)')
+    $('.body-container').css('filter','blur(0.75rem)')
 
     // clicking out of the form will exit
     $('.body-container').click(e => {

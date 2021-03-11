63309d4fcdec151dba9e8c6e8dbc61f91218b659
Sydney Sisco <sydsisco@gmail.com>
2021-03-11 08:06:25 -0800

    remove debug code

--- server.js
+++ server.js
@@ -17,13 +17,6 @@ const { dataFetcher } = require('./db/user-queries');
 const db = require('./lib/db.js');
 db.connect();
 
-console.log('server.js here');
-db.query('select * from items;').then((res) => {
-  console.log('db connection:', res);
-}).catch((res) => {
-  console.log('db connection:', res);
-});
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.

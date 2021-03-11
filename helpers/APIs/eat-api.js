d568ce65de0f35ce748bc62713a2280bba4456d5
Sydney Sisco <sydsisco@gmail.com>
2021-03-10 16:37:57 -0800

    add error handling for yelp api calls

--- helpers/APIs/eat-api.js
+++ helpers/APIs/eat-api.js
@@ -6,21 +6,34 @@ const eatDetails = itemName => {
   const nameEncoded = encodeURI(itemName);
 
   const searchRequest = {
-    term: encodeURI(itemName),
+    term: `${encodeURI(itemName)}`,
     location: 'Vancouver',
     limit: 1,
   };
 
   return client.search(searchRequest).then(response => {
-    const result = response.jsonBody.businesses[0];
-    return {
-      name: result.name,
-      distance: result.distance,
-      rating: result.rating,
-      thumbnail: result.image_url,
-      url: result.url,
+
+    // console.log(response.jsonBody.businesses);
+
+    if (response.jsonBody.businesses.length) {
+      const result = response.jsonBody.businesses[0];
+      const returnData = {
+        name: result.name,
+        distance: result.distance,
+        rating: result.rating,
+        thumbnail: result.image_url,
+        url: result.url,
+      }
+      return returnData;
+
+    } else {
+      return null;
     }
-  }).catch(res => res);
+
+  }).catch(res => {
+    console.log(res);
+    return res;
+  })
 };
 
 module.exports = {

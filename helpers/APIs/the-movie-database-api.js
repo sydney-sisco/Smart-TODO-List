deffbae498b25b42205c2760ae4eb0100bf6f408
Sydney Sisco <sydsisco@gmail.com>
2021-03-10 18:59:35 -0800

    code clean up

--- helpers/APIs/the-movie-database-api.js
+++ helpers/APIs/the-movie-database-api.js
@@ -3,20 +3,13 @@ const axios = require('axios');
 const movieAPIKEY = process.env.THE_MOVIE_DATABASE_API_KEY;
 
 const watchDetails = itemName => {
-
   const movieTitleEncoded = encodeURI(itemName);
-
-  // search for a movie and a tv show
   const movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${movieAPIKEY}&query=${movieTitleEncoded}`
   const showURL = `https://api.themoviedb.org/3/search/tv?api_key=${movieAPIKEY}&query=${movieTitleEncoded}`
 
   const moviePromise = axios.get(movieURL).then(res => {
-    // console.log(res.data.results[0]);
-
     if (res.data.results.length) {
       let movie = res.data.results[0];
-
-      console.log('movie:',movie);
 
       const returnData = {
         title: movie.original_title,
@@ -26,7 +19,6 @@ const watchDetails = itemName => {
         vote_count: movie.vote_count,
         url: `https://www.themoviedb.org/movie/${movie.id}`,
       }
-      // console.log(returnData);
       return returnData;
     } else {
       return null;
@@ -34,12 +26,8 @@ const watchDetails = itemName => {
   })
 
   const showPromise = axios.get(showURL).then(res => {
-    // console.log(res.data.results[0]);
-
     if (res.data.results.length) {
       let show = res.data.results[0];
-
-      console.log('show:',show);
 
       const returnData = {
         title: show.name,
@@ -48,7 +36,6 @@ const watchDetails = itemName => {
         vote_count: show.vote_count,
         url: `https://www.themoviedb.org/tv/${show.id}`,
       }
-      // console.log(returnData);
       return returnData;
     } else {
       return null;
@@ -56,8 +43,6 @@ const watchDetails = itemName => {
   })
 
   return Promise.all([moviePromise, showPromise]).then((results) => {
-    console.log('all values:',results);
-
     const movieResults = results[0];
     const showResults = results[1];
 
@@ -82,7 +67,6 @@ const watchDetails = itemName => {
     console.log(res);
     return res;
   });
-
 };
 
 module.exports = {
